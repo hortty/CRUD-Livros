ed8e1209ae8019fcc8d6860ade1566aeb55bedaf
hortty <gabrielhortmann@gmail.com>
2022-09-21 16:36:52 -0300

    directive and events

--- firstProject/src/app/components/second-component/second-component.component.ts
+++ firstProject/src/app/components/second-component/second-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-second-component',
@@ -7,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecondComponentComponent implements OnInit {
   idades: number[] = [0,1,2,3,4,5];
+  @Input() name: string = "";
+  flag: boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  showMessage(): void{
+    this.flag = !this.flag;
   }
 
 }
