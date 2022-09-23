dea782ad410af40b0083ffdd5db1c1beebe37f46
hortty <gabrielhortmann@gmail.com>
2022-09-22 22:57:30 -0300

    class Book and BookContext

---
+++ Books/Books/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Books.Models
+{
+    public class Book
+    {
+        public int id { get; set; } 
+        public string title { get; set; }   
+        public int price { get; set; }
+        public string descricao { get; set; }   
+    }
+}
