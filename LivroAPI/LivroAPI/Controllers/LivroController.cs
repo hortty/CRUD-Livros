63621f421e37f0db36249b345dba383710897a87
Gabriel Bueno <gabrielhortmann@gmail.com>
2022-10-01 18:53:05 -0300

    LivroAPI
    .net core

---
+++ LivroAPI/LivroAPI/Controllers/LivroController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LivroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Livro>>> GetLivro()
+        {
+            return new List<Livro>
+            {
+                new Livro
+                {
+                    titulo_produto = "Harry Poter Calice de Fogo",
+                    descricao_produto = "Livro bom",
+                    preco_produto = 78.89,
+                    id = 75
+                }
+            };
+        }
+    }
+}
---
+++ LivroAPI/LivroAPI/Livro.cs
@@ -0,0 +1,11 @@
+﻿namespace LivroAPI
+{
+    public class Livro
+    {
+        public int id { get; set; }
+        public string titulo_produto { get; set; }
+        public string descricao_produto { get; set; }
+        public double preco_produto { get; set; }
+
+    }
+}
