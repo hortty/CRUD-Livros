cb9e71724761cc265da69e7c781578fe3a49b5b5
Gabriel Bueno <gabrielhortmann@gmail.com>
2022-10-10 00:54:11 -0300

    model book
    book model added

---
+++ LivroAPI/LivroAPI/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Book
+{
+	public Book(int id, string nome, string descricao)
+	{
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+	}
+
+    public int id{ get; set; }
+
+    public string nome { get; set; }
+
+    public string descricao { get; set; }
+
+}
